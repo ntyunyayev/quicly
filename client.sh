d2b657736fbdfdc769ed578831643b427a33bc70
kikito <nikita.tyunyayev@student.uclouvain.be>
2022-10-31 14:31:25 +0000

    small fix

--- client.sh
+++ client.sh
@@ -1,2 +1,2 @@
 #sudo perf record --call-graph dwarf -o /home/nikita/memoire/dpdk_picoquic/perfMeasures/perf_quicly_client.data taskset -c 4 ./cli -p /20000000000 10.100.0.2 4433 -O -b 20000000000 -G
-sudo ./cli -p /20000000000 10.100.0.2 4433 -O -b 20000000000 -G
+sudo ./cli_quicly -p /20000000000 10.100.0.2 4433 -O -b 20000000000 -G
--- server.sh
+++ server.sh
@@ -1,2 +1,2 @@
 #sudo perf record --call-graph dwarf -o /home/nikita/memoire/dpdk_picoquic/perfMeasures/perf_quicly_server.data taskset -c 4 ./cli -c server.cert -k server.key 10.100.0.2 4433 -b 20000000000 -G
-sudo ./cli -c server.cert -k server.key 10.100.0.2 4433 -b 20000000000 -G
+sudo ./cli_quicly -c server.cert -k server.key 10.100.0.2 4433 -b 20000000000 -G
